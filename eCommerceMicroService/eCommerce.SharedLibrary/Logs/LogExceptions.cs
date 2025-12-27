7e9750db6925d92a7b99f843dc13dc5339cacf42
Kiranshankarbhat007 <kiranshankarbhatr@gmail.com>
2025-12-27 22:50:12 +0530

    Common shared library for all others projects

---
+++ eCommerceMicroService/eCommerce.SharedLibrary/Logs/LogExceptions.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Logs
+{
+    public static class LogExceptions
+    {
+        public static void LogException(Exception ex)
+        {
+            LogToFile(ex.Message);
+            LogToConsole(ex.Message);
+            LogToDebug(ex.Message);
+        }
+
+        private static void LogToFile(string message) => Log.Information(message);
+
+        private static void LogToConsole(string message) => Log.Warning(message);
+
+        private static void LogToDebug(string message) => Log.Debug(message);
+    }
+}
