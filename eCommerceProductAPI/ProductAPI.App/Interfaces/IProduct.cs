b3dbf36fca8ed40dca7113953802b39a73f6f3da
Kiranshankarbhat007 <kiranshankarbhatr@gmail.com>
2026-03-04 22:24:22 +0530

    Second Commit

---
+++ eCommerceProductAPI/ProductAPI.App/Interfaces/IProduct.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.SharedLibrary.Interface;
+using ProductAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.App.Interfaces
+{
+    public interface IProduct : IGenericInterface<Product>
+    {
+    }
+}
---
+++ eCommerceProductAPI/ProductAPI.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Domain.Entities
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal StockQuantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
